677dcc5d0d4cf090ad6e508caa91e52c87c23acb
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2022-06-13 15:43:10 +0000

    Added General Public License.

--- load.h
+++ load.h
@@ -1,3 +1,11 @@
+// /CubedProgrammer/TypingTest, a simple game for checking your typing speed
+// Copyright (C) 2022 github.com/CubedProgrammer, owner of said account
+
+// This file is part of /CubedProgrammer/TypingTest
+// /CubedProgrammer/TypingTest is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// /CubedProgrammer/TypingTest is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with /CubedProgrammer/TypingTest. If not, see <https://www.gnu.org/licenses/>.
+
 #ifndef LOAD_H_
 #define LOAD_H_
 #include<stddef.h>
